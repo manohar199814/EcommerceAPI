747627804d4560ded9ddcbba2f1870f7708598b2
john doe <manoharmanu2614@gmail.com>
2022-12-03 19:20:51 +0530

    changed mongodb connect url

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 main().catch((err)=> {console.log(err)});
 
 async function main() {
-    await mongoose.connect(`mongodb://127.0.0.1/ecommerceAPI`);
+    await mongoose.connect(`mongodb://0.0.0.0:27017/ecommerceAPI`);
     console.log('connection successful to DB');
 }
